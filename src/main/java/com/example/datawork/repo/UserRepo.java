432688bc747e7e2b8e6244de1ce89afc1608317a
loldude <gleb22002e@mail.ru>
2022-09-09 00:26:26 +0300

    added annotation

--- src/main/java/com/example/datawork/repo/UserRepo.java
+++ src/main/java/com/example/datawork/repo/UserRepo.java
@@ -2,10 +2,12 @@ package com.example.datawork.repo;
 
 import com.example.datawork.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepo extends JpaRepository<UserEntity, Integer> {
     List<UserEntity> findByNickOrGitOrAboutMe(String nick, String git, String about_me);
     List<UserEntity> findByAgeOrWorkHoursOrId(Integer age, Integer work_hours, Integer number);
